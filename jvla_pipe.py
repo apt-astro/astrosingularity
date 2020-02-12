1f335f4dfaf35f5d8f8f7883f3e0621eb4e58998
apt-astro <59600568+apt-astro@users.noreply.github.com>
2020-02-12 17:43:09 +0000

    Delete jvla_pipe.py

--- jvla_pipe.py
+++
@@ -1,8 +0,0 @@
-import glob, os
-import pipeline.recipes.hifv as hifv
-infile = glob.glob("*.tar")
-os.system("tar -xvf "+infile[0])
-os.system("rm "+infile[0])
-fileuntar = os.path.splitext(infile[0])
-hifv.hifv([fileuntar[0]])
-os.system("rm -r "+fileuntar[0])
